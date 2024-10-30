75669702d267540ecedbeb9b270f5f69fa786638
Gia-gia9 <giadagelashvili@gmail.com>
2024-10-30 23:54:58 +0400

    sum

---
+++ script.js
@@ -0,0 +1,11 @@
+let name1 = "Gia"
+let surname = "Dagelashivli"
+let age1 = 24
+let email = "giadagelashvili@gmail.com"
+let phonenumber = 551134755
+let arissrulwlovani = true
+let ararissrulwlovani = false
+
+console.log("my name is", name1, "surname", surname, "I am", age1, " my email is", email, "and phone number", phonenumber, arissrulwlovani)
+console.log("%c arissrulwlovani", "color:yellow")
+console.log("%c 551134755", "color:red")
